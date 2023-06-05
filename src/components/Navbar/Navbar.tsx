6810fe88115de9ead82fea800e19846b41e14278
Edwin <miguepinchao1992@gmail.com>
2023-06-05 08:54:28 -0500

    configuracion de eslint y prettier, editconfig

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,20 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
-import React from 'react';
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import React from "react";
 
-export type NavbarProps = {
-}
+export type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-		<AppBar position="fixed">
-		  <Toolbar>
-			
-			<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-			  Inicio Clean REact App
-			</Typography>
-			
-		  </Toolbar>
-		</AppBar>
-	  </Box>
-	);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Inicio Clean REact App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navbar;
