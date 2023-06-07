0b616e3034747b9eb86a179919caf2352828134b
Edwin <miguepinchao1992@gmail.com>
2023-06-07 04:42:39 -0500

    Add Footer component, agregando reglas eslint

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface FooterProps {
+  name: string;
+}
+
+const Footer: React.FC<FooterProps> = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export * from './GridFlow';
-export * from './Navbar';
+export * from "./Footer";
+export * from "./GridFlow";
+export * from "./Navbar";
